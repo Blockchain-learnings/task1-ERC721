9bf1475466e39066c1ed8b03f7aa38bd60721d07
ayushThakur747 <itsathakur30@gmail.com>
2022-11-01 09:54:21 +0530

    add getNFTsForOwner function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract ATNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _supply;
+    mapping(address => mapping(uint256 => uint256)) private _ownersNFTs;
 
     event Minted(address _from, uint256 _id, string _tokenURI);
 
@@ -21,6 +22,9 @@ contract ATNFT is ERC721URIStorage {
 
         uint256 tokenId = _supply.current();
         _mint(msg.sender, tokenId);
+
+        uint256 _ownerBalance = balanceOf(msg.sender) + 1;
+        _ownersNFTs[msg.sender][_ownerBalance] = tokenId;
 
         _setTokenURI(tokenId, _tokenURI);
 
@@ -39,6 +43,19 @@ contract ATNFT is ERC721URIStorage {
             size := extcodesize(_address)
         }
         return (size > 0); //Warning: will return false if the call is made from the constructor of a smart contract
+    }
+
+    function getNFTsForOwner(address walletAddress)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory NFTs;
+        uint256 ownerBalance = balanceOf(walletAddress);
+        for (uint256 i = 1; i <= ownerBalance; i++) {
+            NFTs[i] = _ownersNFTs[walletAddress][i];
+        }
+        return NFTs;
     }
 }
 
