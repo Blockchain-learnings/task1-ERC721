781577042fba96d4d3105adf7a345365751db587
ayushThakur747 <itsathakur30@gmail.com>
2022-11-28 10:53:00 +0530

    add deploy.json, update test scripts deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,12 @@ module.exports = {
   solidity: "0.8.17",
   defaultNetwork: "localhost",
   networks: {
-    hardhat: {},
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [`${process.env.ACCOUNT_PRIVATE_KEY}`],
+    },
+    "truffle-dashboard": {
+      url: "http://localhost:24012/rpc",
+    },
   },
 };
--- test/ERC721.js
+++ test/ERC721.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
 
 describe("ERC721 contract", () => {
   let owners, acc1, acc2;
@@ -34,22 +34,23 @@ describe("ERC721 contract", () => {
   describe("mint functionalities", async () => {
     it("should fail if less than 0.5 eth provided", async () => {
       await expect(
-        deployedContract.mint("", { value: "1000000000000000" })
+        deployedContract.mint("", { value: ethers.utils.parseEther("0.1") })
       ).to.be.revertedWith("0.5 eth required to mint");
     });
 
     it("should fail if more than 0.5 eth provided", async () => {
       await expect(
-        deployedContract.mint("", { value: "600000000000000000" })
+        deployedContract.mint("", { value: ethers.utils.parseEther("0.6") })
       ).to.be.revertedWith("0.5 eth required to mint");
     });
 
     it("should pass if 0.5 eth provided", async () => {
       await deployedContract
         .connect(acc2)
-        .mint("", { value: "500000000000000000" });
+        .mint("", { value: ethers.utils.parseEther("0.5") });
       const balance = await deployedContract.balanceOf(acc2.address);
-      await expect(BigNumber.from(balance)).to.equal(1);
+      console.log("balance>>", balance);
+      expect(balance).to.equal(1);
     });
 
     it("should revert when called from contract", async () => {
@@ -61,9 +62,9 @@ describe("ERC721 contract", () => {
     it("list owner's nfts", async () => {
       await deployedContract
         .connect(acc2)
-        .mint("", { value: "500000000000000000" });
+        .mint("", { value: ethers.utils.parseEther("0.5") });
       const value = await deployedContract.getNFTsForOwner(acc2.address);
-      expect(BigNumber.from(value[0])).to.equal(1);
+      expect(value[0]).to.equal(1);
     });
   });
 });
